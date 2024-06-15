bd0277ae8552039cd20c176d639ca44c9f17ecc7
rafael <rafaelmagalhaes1@live.com>
2024-06-15 20:00:49 -0300

    feat: Person entity created

---
+++ src/main/java/com/api/auth/entity/Person.java
@@ -0,0 +1,182 @@
+package com.api.auth.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import java.util.Collection;
+import java.util.List;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+/**
+ * The type Person.
+ */
+@Entity
+@Table(name = "persons")
+public class Person implements UserDetails {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String fullName;
+  private String username;
+
+  @Column(unique = true)
+  private String email;
+  private String password;
+  private String role;
+
+  /**
+   * Instantiates a new Person.
+   */
+  public Person() { }
+
+  /**
+   * Instantiates a new Person.
+   *
+   * @param fullName the full name
+   * @param username the username
+   * @param email    the email
+   * @param password the password
+   * @param role     the role
+   */
+  public Person(String fullName, String username, String email, String password, String role) {
+    this.fullName = fullName;
+    this.username = username;
+    this.email = email;
+    this.password = password;
+    this.role = role;
+  }
+
+  /**
+   * Gets id.
+   *
+   * @return the id
+   */
+  public Long getId() {
+    return id;
+  }
+
+  /**
+   * Sets id.
+   *
+   * @param id the id
+   */
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  /**
+   * Gets full name.
+   *
+   * @return the full name
+   */
+  public String getFullName() {
+    return fullName;
+  }
+
+  /**
+   * Sets full name.
+   *
+   * @param fullName the full name
+   */
+  public void setFullName(String fullName) {
+    this.fullName = fullName;
+  }
+
+  /**
+   * Gets email.
+   *
+   * @return the email
+   */
+  public String getEmail() {
+    return email;
+  }
+
+  /**
+   * Sets email.
+   *
+   * @param email the email
+   */
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  /**
+   * Gets role.
+   *
+   * @return the role
+   */
+  public String getRole() {
+    return role;
+  }
+
+  /**
+   * Sets role.
+   *
+   * @param role the role
+   */
+  public void setRole(String role) {
+    this.role = role;
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return List.of(new SimpleGrantedAuthority(role));
+  }
+
+  /**
+   * Sets password.
+   *
+   * @param password the password
+   */
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  @Override
+  public String getPassword() {
+    return this.password;
+  }
+
+  /**
+   * Sets username.
+   *
+   * @param username the username
+   */
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  @Override
+  public String getUsername() {
+    return this.username;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
+}
