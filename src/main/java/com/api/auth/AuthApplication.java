dcac2ad5b393354ef7bfa6839e63d8587a556251
rafael <rafaelmagalhaes1@live.com>
2024-06-15 20:00:10 -0300

    initial commit

--- src/main/java/com/api/auth/AuthApplication.java
+++ src/main/java/com/api/auth/AuthApplication.java
@@ -3,9 +3,17 @@ package com.api.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The type Auth application.
+ */
 @SpringBootApplication
 public class AuthApplication {
 
+	/**
+	 * The entry point of application.
+	 *
+	 * @param args the input arguments
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(AuthApplication.class, args);
 	}
