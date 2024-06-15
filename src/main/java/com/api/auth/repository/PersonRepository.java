cdc201290a5927420cc73dbda56bec92fdabfc01
rafael <rafaelmagalhaes1@live.com>
2024-06-15 20:01:12 -0300

    feat: Person repository created

---
+++ src/main/java/com/api/auth/repository/PersonRepository.java
@@ -0,0 +1,29 @@
+package com.api.auth.repository;
+
+import com.api.auth.entity.Person;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * The interface Person repository.
+ */
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long> {
+
+  /**
+   * Find by username optional.
+   *
+   * @param username the username
+   * @return the optional
+   */
+  Optional<Person> findByUsername(String username);
+
+  /**
+   * Find a person by email.
+   *
+   * @param email the email of the person
+   * @return an Optional containing the person if found, or empty otherwise
+   */
+  Optional<Person> findByEmail(String email);
+}
