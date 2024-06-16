c0cb3e80fed4fb34b08c45cfd9ded5b469066f55
rafael <rafaelmagalhaes1@live.com>
2024-06-16 13:06:30 -0300

    refact: Person entity, add UUID on ids

--- src/main/java/com/api/auth/entity/Person.java
+++ src/main/java/com/api/auth/entity/Person.java
@@ -6,11 +6,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import java.util.Collection;
 import java.util.List;
+import java.util.UUID;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -23,8 +21,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 public class Person implements UserDetails {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+  @GeneratedValue
+  private UUID id;
   private String fullName;
   private String username;
 
@@ -60,7 +58,7 @@ public class Person implements UserDetails {
    *
    * @return the id
    */
-  public Long getId() {
+  public UUID getId() {
     return id;
   }
 
@@ -69,7 +67,7 @@ public class Person implements UserDetails {
    *
    * @param id the id
    */
-  public void setId(Long id) {
+  public void setId(UUID id) {
     this.id = id;
   }
 
